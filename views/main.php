530ce50855e709b62f88a813c08652eb440d1db5
Pedro Centeno Lopez <pedroiclop25@gmail.com>
2024-12-09 19:53:08 -0600

    Agregado de info a la pagina principal

--- views/main.php
+++ views/main.php
@@ -42,15 +42,15 @@ include_once 'header.php';
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <h2 class="text-primary font-weight-medium m-0">Hemos Servido</h2>
                         <h1 class="display-1 text-white m-0">Café</h1>
-                        <h2 class="text-white m-0">* Desde ? *</h2>
+                        <h2 class="text-white m-0">Desde 1950</h2>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img class="w-100" src="img/carousel-2.jpg" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                        <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
-                        <h1 class="display-1 text-white m-0">COFFEE</h1>
-                        <h2 class="text-white m-0">* SINCE 1950 *</h2>
+                        <h2 class="text-primary font-weight-medium m-0">Es un placer servirles</h2>
+                        <h1 class="display-1 text-white m-0">CAFE</h1>
+                        <h2 class="text-white m-0">Desde 1950</h2>
                     </div>
                 </div>
             </div>
@@ -69,14 +69,14 @@ include_once 'header.php';
     <div class="container-fluid py-5">
         <div class="container">
             <div class="section-title">
-                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">About Us</h4>
-                <h1 class="display-4">Serving Since 1950</h1>
+                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Sobre Nosotros</h4>
+                <h1 class="display-4">Sirviendo desde 1950</h1>
             </div>
             <div class="row">
                 <div class="col-lg-4 py-0 py-lg-5">
-                    <h1 class="mb-3">Our Story</h1>
-                    <h5 class="mb-3">Eos kasd eos dolor vero vero, lorem stet diam rebum. Ipsum amet sed vero dolor sea</h5>
-                    <p>Takimata sed vero vero no sit sed, justo clita duo no duo amet et, nonumy kasd sed dolor eos diam lorem eirmod. Amet sit amet amet no. Est nonumy sed labore eirmod sit magna. Erat at est justo sit ut. Labor diam sed ipsum et eirmod</p>
+                    <h1 class="mb-3">Nuestra historia</h1>
+                    <h5 class="mb-3">Desde primeras horas de la mañana nuestra cocina abierta desprende los olores de una comida fresca y generosa.</h5>
+                    <p>En un ambiente cálido y desentendido brindamos desayunos, almuerzos y café con una atención cariñosa y cercana.</p>
                     <a href="" class="btn btn-secondary font-weight-bold py-2 px-4 mt-2">Learn More</a>
                 </div>
                 <div class="col-lg-4 py-5 py-lg-0" style="min-height: 500px;">
@@ -85,8 +85,10 @@ include_once 'header.php';
                     </div>
                 </div>
                 <div class="col-lg-4 py-0 py-lg-5">
-                    <h1 class="mb-3">Our Vision</h1>
-                    <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor</p>
+                    <h1 class="mb-3">Lo hecho en casa es mejor</h1>
+                    <p>Comida sana es comida con conciencia. Los alimentos naturales y orgánicos están al alcance de todos, y es nuestra misión acercarle a ellos.
+                        Además, se evitará los agroquímicos y preservantes tan presentes  en la industria alimenticia. ¡Sí se puede ser consciente y contar con opciones!
+                        Mientras más nos unamos a la causa, menores serán los costos para los productores, ¡y todos salimos ganando!</p>
                     <h5 class="mb-3"><i class="fa fa-check text-primary mr-3"></i>Lorem ipsum dolor sit amet</h5>
                     <h5 class="mb-3"><i class="fa fa-check text-primary mr-3"></i>Lorem ipsum dolor sit amet</h5>
                     <h5 class="mb-3"><i class="fa fa-check text-primary mr-3"></i>Lorem ipsum dolor sit amet</h5>
@@ -102,8 +104,8 @@ include_once 'header.php';
     <div class="container-fluid pt-5">
         <div class="container">
             <div class="section-title">
-                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Our Services</h4>
-                <h1 class="display-4">Fresh & Organic Beans</h1>
+                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Nuestros Servicios</h4>
+                <h1 class="display-4"></h1>
             </div>
             <div class="row">
                 <div class="col-lg-6 mb-5">
